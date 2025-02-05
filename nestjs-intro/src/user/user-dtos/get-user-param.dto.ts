dd210799e39eb23f9f8db68f64f312f689e7f280
Heber Solano <hebersolano1@gmail.com>
2025-02-05 09:17:25 -0600

    user providers

--- nestjs-intro/src/user/user-dtos/get-user-param.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
-import { Type } from 'class-transformer';
-export class GetUserParamDto {
-  @IsInt()
-  @IsNotEmpty()
-  @Type(() => Number)
-  id: number;
-}
