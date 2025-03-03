e129d576280386d3bbf5e8e975d1eeea0cca7aee
Heber Solano <hebersolano1@gmail.com>
2025-03-02 21:56:51 -0600

    mail module 0.2

--- nestjs-intro/src/mail/mail.module.ts
+++ nestjs-intro/src/mail/mail.module.ts
@@ -10,32 +10,37 @@ import { EjsAdapter } from "@nestjs-modules/mailer/dist/adapters/ejs.adapter";
   imports: [
     MailerModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService): MailerOptions => ({
-        transport: {
-          hostname: config.get<string>("appConfig.mailHost"),
-          secure: false,
-          port: 587,
-          auth: {
-            user: config.get<string>("appConfig.smtpUsername"),
-            pass: config.get<string>("appConfig.smtpPassword"),
+      useFactory: (config: ConfigService): MailerOptions => {
+        console.log(
+          ">>test mail config: ",
+          config.get<string>("appConfig.mailHost"),
+        );
+        return {
+          transport: {
+            host: config.get<string>("appConfig.mailHost"),
+            secure: false,
+            port: 2525,
+            auth: {
+              user: config.get<string>("appConfig.smtpUsername"),
+              pass: config.get<string>("appConfig.smtpPassword"),
+            },
+            logger: true,
+            debug: true,
           },
-          logger: true,
-          debug: true,
-          tls: {
-            rejectUnAuthorized: true,
+          defaults: {
+            from: `My nestjs blog <no-reply@my-blog.com>`,
           },
-        },
-        defaults: {
-          from: `My nestjs blog <no-reply@my-blog.com>`,
-        },
-        template: {
-          dir: join(__dirname, "templates"),
-          adapter: new EjsAdapter(),
-          options: {
-            strict: false,
+          template: {
+            dir: join(__dirname, "templates"),
+            adapter: new EjsAdapter({
+              inlineCssEnabled: true,
+            }),
+            options: {
+              strict: false,
+            },
           },
-        },
-      }),
+        };
+      },
     }),
   ],
   providers: [MailService],
